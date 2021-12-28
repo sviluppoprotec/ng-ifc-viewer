2809b0460e4b04dfee77fee1231e87f0d6544043
Emile Grillot de Givry <federico.pranio@gmail.com>
2021-12-29 00:36:37 +0100

    caricamento file ifc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { IFCCOLUMN, IFCOPENINGELEMENT, IFCROOF, IFCSLAB, IFCSPACE, IFCSTAIR, IFCWALL, IFCWALLSTANDARDCASE } from 'web-ifc';
 import { IfcViewerAPI } from '../viewer/dist/ifc-viewer-api'
 import { MeshBasicMaterial, LineBasicMaterial, Color, EdgesGeometry } from 'three';
+import { createSideMenuButton } from './classes/gui-creator';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,38 @@ export class AppComponent implements AfterViewInit {
   title = 'ifc-xbim';
   viewer: IfcViewerAPI;
   fills = [];
-
+  model;
   constructor() {
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.setViewer();
-    
+
+    //Setup UI
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'file');
+    inputElement.classList.add('hidden');
+    inputElement.addEventListener('change', this.loadIfc, false);
+    document.body.appendChild(inputElement);
+
+    const loadButton = createSideMenuButton('./assets/resources/folder-icon.svg');
+    loadButton.addEventListener('click', () => {
+      loadButton.blur();
+      inputElement.click();
+    });
+
+    const sectionButton = createSideMenuButton('./assets/resources/section-plane-down.svg');
+    sectionButton.addEventListener('click', () => {
+      sectionButton.blur();
+      this.viewer.toggleClippingPlanes();
+    });
+
+    const dropBoxButton = createSideMenuButton('./assets/resources/dropbox-icon.svg');
+    dropBoxButton.addEventListener('click', () => {
+      dropBoxButton.blur();
+      this.viewer.openDropboxWindow();
+    });
+
   }
 
   setViewer() {
@@ -79,5 +105,32 @@ export class AppComponent implements AfterViewInit {
     material.polygonOffsetUnits = 1;
     this.fills.push(this.viewer.fills.create(`${modelID}`, modelID, ids, material));
   }
+
+  loadIfc = async (event) => {
+    const overlay = document.getElementById('loading-overlay');
+    const progressText = document.getElementById('loading-progress');
+  
+    overlay.classList.remove('hidden');
+    progressText.innerText = `Loading`;
+  
+    this.viewer.IFC.loader.ifcManager.setOnProgress((event) => {
+      const percentage = Math.floor((event.loaded * 100) / event.total);
+      progressText.innerText = `Loaded ${percentage}%`;
+    });
+  
+    this.viewer.IFC.loader.ifcManager.parser.setupOptionalCategories({
+      [IFCSPACE]: false,
+      [IFCOPENINGELEMENT]: false
+    });
+  
+    this.model = await this.viewer.IFC.loadIfc(event.target.files[0], true);
+    this.model.material.forEach(mat => mat.side = 2);
+  
+    this.createFill(this.model.modelID);
+    // viewer.edges.create(`${model.modelID}`, model.modelID, lineMaterial);
+    // viewer.edges.toggle(`${model.modelID}`);
+  
+    overlay.classList.add('hidden');
+  };
 
 }
